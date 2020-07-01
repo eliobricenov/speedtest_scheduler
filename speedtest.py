03318b12eb5a1920efc1f221d9b4d29835da4013
Elio <eliobricenov@gmail.com>
2020-07-01 19:03:55 -0400

    added speedtest function to be used in job

---
+++ speedtest.py
@@ -0,0 +1,40 @@
+import fastdotcom
+import traceback
+import os
+
+from logger import get_logger
+
+EXPECTED_SPEED_MB = 2
+MAX_TRIES_NUMBER = 3
+
+
+def main():
+    logger = get_logger()
+    result = {}
+    logger.info('STARTING SPEED TEST IN PID: {}'.format(os.getpid()))
+
+    try:
+        result = run_test()
+    except SystemError:
+        error = traceback.print_exc()
+        print('ERROR: {}', error)
+        logger.error(error)
+
+    logger.info('TEST RESULT: {}'.format(result))
+
+
+def run_test():
+    print('Running test ...')
+    num_tries = 0
+    test_result = fastdotcom.fast_com()
+
+    while test_result == 0 and num_tries < MAX_TRIES_NUMBER:
+        num_tries += 1
+        test_result = fastdotcom.fast_com()
+
+    print('Test result: {} MB'.format(test_result))
+    return test_result
+
+
+if __name__ == '__main__':
+    main()
