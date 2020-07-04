6310bff7b8ffbcc16f143225779e27e270e6d297
Elio <eliobricenov@gmail.com>
2020-07-04 13:56:53 -0400

    improved util function to get file path

--- src/util.py
+++ src/util.py
@@ -1,7 +1,6 @@
-import os
-import sys
+import pathlib
 
 
-def get_script_path():
-    return os.path.dirname(os.path.realpath(sys.argv[0]))
+def get_script_path(file):
+    return pathlib.Path(file).parent.absolute()
 
